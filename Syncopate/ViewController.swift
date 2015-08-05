3fa293e4e739e223f08b38730d3a875d6e3ca14c
Gary Chang <gwh.chang@gmail.com>
2015-08-05 10:49:36 +0800

    Code to submit json get request

--- Syncopate/ViewController.swift
+++ Syncopate/ViewController.swift
@@ -13,7 +13,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var dataSourceLabel: UILabel!
     @IBOutlet weak var dataSourceTextField: UITextField!
 
-    @IBOutlet weak var dataSourceButton: UIButton!
+    @IBOutlet weak var startButton: UIButton!
+    @IBOutlet weak var stopButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -23,18 +24,44 @@ class ViewController: UIViewController {
         
         self.dataSourceTextField.text = self.dataSourceLabel.text;
         
-        self.dataSourceButton.setTitle("Submit", forState: .Normal);
-        self.dataSourceButton.addTarget(self, action: "handleDataSourceButtonClick:", forControlEvents: .TouchUpInside);
+        self.startButton.setTitle("Start", forState: .Normal);
+        self.startButton.addTarget(self, action: "handleStartButtonClick:", forControlEvents: .TouchUpInside);
+        
+        self.stopButton.setTitle("Stop", forState: .Normal);
+        self.stopButton.addTarget(self, action: "handleStopButtonClick:", forControlEvents: .TouchUpInside);
     }
     
-    func handleDataSourceButtonClick(sender:UIButton!) {
-        println("Button pressed");
+    func handleStartButtonClick(sender:UIButton!) {
         self.dataSourceLabel.text = self.dataSourceTextField.text;
     }
     
+    func handleStopButtonClick(sender:UIButton!) {
+
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func getData() {
+        var url : String = self.dataSourceLabel.text!;
+        var request : NSMutableURLRequest = NSMutableURLRequest()
+        request.URL = NSURL(string: url)
+        request.HTTPMethod = "GET"
+        
+        NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue(), completionHandler:{ (response:NSURLResponse!, data: NSData!, error: NSError!) -> Void in
+            var error: AutoreleasingUnsafeMutablePointer<NSError?> = nil
+            let jsonResult: NSDictionary! = NSJSONSerialization.JSONObjectWithData(data, options:NSJSONReadingOptions.MutableContainers, error: error) as? NSDictionary
+            
+            if (jsonResult != nil) {
+                // process jsonResult
+            } else {
+                // couldn't load JSON, look at error
+            }
+            
+            println(request.URL);
+            
+        })
     }
 
 
