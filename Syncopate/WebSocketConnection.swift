8d87831a1abc0723738ac4f942f70b75723da5eb
Gary Chang <gwh.chang@gmail.com>
2015-09-04 14:36:38 -0500

    web socket class

---
+++ Syncopate/WebSocketConnection.swift
@@ -0,0 +1,86 @@
+//
+//  WebSocketConnection.swift
+//  Syncopate
+//
+//  Created by Gary Chang on 9/4/15.
+//  Copyright (c) 2015 Gary Chang. All rights reserved.
+//
+
+import Foundation
+import Starscream
+
+class WebSocketConnection: WebSocketDelegate {
+    
+    // MARK: Properties
+    var host: String
+    var path: String
+    var socket: WebSocket?
+    var received: Int64
+    
+    init() {
+        self.received = 0
+        self.host = ""
+        self.path = ""
+        self.socket = nil
+    }
+    
+    func connect(host: String, path:String) {
+        self.received = 0
+        self.host = host
+        self.path = path
+        self.socket = WebSocket(url: NSURL(scheme: "ws",
+            host: self.host,
+            path: self.path)!);
+        self.socket?.delegate = self;
+        self.socket?.connect();
+        
+        println("Connecting to websocket: \(self.host)\(self.path)")
+    }
+    
+    func disconnect() {
+        self.socket?.disconnect()
+        self.received = 0
+        
+        println("Disconnecting to websocket: \(self.host)\(self.path)")
+    }
+    
+    // MARK: Websocket callback
+    func onMessage(data: NSDictionary) {
+        
+    }
+    
+    // MARK: Websocket delegate methods
+    func websocketDidConnect(ws: WebSocket) {
+        println("Websocket is connected: \(self.host)\(self.path)")
+    }
+    
+    func websocketDidDisconnect(ws: WebSocket, error: NSError?) {
+        if let e = error {
+            println("Websocket is disconnected: \(e.localizedDescription)")
+        } else {
+            println("Websocket disconnected: \(self.host)\(self.path)")
+        }
+    }
+    
+    func websocketDidReceiveMessage(ws: WebSocket, text: String) {
+        self.received++
+        
+        if let data = text.dataUsingEncoding(NSUTF8StringEncoding) {
+            var error: NSError?;
+            let jsonResult: NSDictionary! = NSJSONSerialization.JSONObjectWithData(data,
+                options:NSJSONReadingOptions.allZeros,
+                error: &error) as? NSDictionary;
+            
+            if (jsonResult != nil) {
+                self.onMessage(jsonResult)
+            } else {
+                println("ERROR: Unable to parse text: \(text)");
+            }
+        }
+    }
+    
+    func websocketDidReceiveData(ws: WebSocket, data: NSData) {
+        println("Received data: \(data.length)")
+    }
+
+}
