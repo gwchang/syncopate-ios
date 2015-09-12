5b6d335dd85051ce079840c7f563d62d7db91d1a
Gary Chang <gwh.chang@gmail.com>
2015-09-11 19:25:32 -0500

    login view controller

---
+++ Syncopate/LoginTableView.swift
@@ -0,0 +1,21 @@
+//
+//  LoginTableView.swift
+//  Syncopate
+//
+//  Created by Gary Chang on 9/11/15.
+//  Copyright (c) 2015 Gary Chang. All rights reserved.
+//
+
+import UIKit
+
+class LoginTableView: UITableView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Syncopate/LoginViewController.swift
@@ -0,0 +1,48 @@
+//
+//  LoginViewController.swift
+//  Syncopate
+//
+//  Created by Gary Chang on 9/11/15.
+//  Copyright (c) 2015 Gary Chang. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    @IBOutlet weak var welcomeLabel: UILabel!
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        self.welcomeLabel.textColor = SyncopateStyle.menuTextColor
+        self.view.backgroundColor = SyncopateStyle.menuBackgroundColor
+        
+        usernameTextField.placeholder = "Username";
+        usernameTextField.autocorrectionType = UITextAutocorrectionType.No;
+        usernameTextField.clearButtonMode = UITextFieldViewMode.WhileEditing
+        
+        passwordTextField.placeholder = "Password";
+        passwordTextField.autocorrectionType = UITextAutocorrectionType.No;
+        passwordTextField.clearButtonMode = UITextFieldViewMode.WhileEditing
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
