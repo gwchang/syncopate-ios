58d59f41587b8aba7ab9e2fc7f6f32bae24e32fc
Gary Chang <gwh.chang@gmail.com>
2015-09-17 12:14:21 -0500

    header cell

---
+++ Syncopate/HeaderTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  HeaderTableViewCell.swift
+//  Syncopate
+//
+//  Created by Gary Chang on 9/17/15.
+//  Copyright (c) 2015 Gary Chang. All rights reserved.
+//
+
+import UIKit
+
+class HeaderTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var headerLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
